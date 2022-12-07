e62c20b08ce50f7cfd3d19601d5ae187da6ab2ed
LazorykVlad <vladyslav.lazoryk@cyper.ca>
2022-12-07 11:40:09 +0200

    refactoring

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 
 function Forecast(props) {
     let forecast = props.forecast
-    console.log(forecast)
 
     return (
         <>
             <div className="day-item">
+                {forecast.temp.day}&deg;
             </div>
         </>
     )
--- src/components/SearchMain.js
+++ src/components/SearchMain.js
@@ -34,13 +34,18 @@ function SearchMain() {
           sunset
         }
 
+        getForecastDetails(lon, lat)
         setTempInfo(myNewWeatherInfo)
 
       } catch (error) {
         //console.log(error)
       }
+    }
+
+    const getForecastDetails = async(lon, lat) => {
+
       try {
-            let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,current&appid=edfbb71f85e1fcfb2f25c50ebd685682`
+            let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,current&units=metric&appid=edfbb71f85e1fcfb2f25c50ebd685682`
             let res = await fetch(url);
             let forecast_data = await res.json();
 
@@ -48,29 +53,32 @@ function SearchMain() {
         } catch (error) {
             console.log(error)
         }
-
     }
 
     useEffect(() =>{
       getWeatherDetails()
     }, [])
 
-    // const handleKeyDown = (event) => {
-    // if (event.key === 'Enter') {
-    //     getWeatherDetails()
-    // }
-  // }
+    const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+        getWeatherDetails()
+    }
+  }
 
   return (
    <> <header>Text</header>
        <div className='wrap'>
       <div className='search'>
-          <input className="searchTerm" type='search' placeholder='Type city' id='search' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+          <input className="searchTerm" onKeyDown={handleKeyDown} type='search' placeholder='Type city' id='search' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
       <button className='searchButton' onClick={getWeatherDetails}>Search</button>
       </div>
     </div>
        <div className="days">
-        <Forecast forecast={forecastData}/>
+        <Forecast forecast={forecastData.daily[0]}/>
+        <Forecast forecast={forecastData.daily[1]}/>
+        <Forecast forecast={forecastData.daily[2]}/>
+        <Forecast forecast={forecastData.daily[3]}/>
+        <Forecast forecast={forecastData.daily[4]}/>
            </div>
     <WeatherDetails {...tempInfo} /></>
   )
